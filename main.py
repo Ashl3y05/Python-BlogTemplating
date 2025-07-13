cbfc8484feb5588dd6f53870bd6febd3b0d6ad59
Ashley <panelo.ashley@gmail.com>
2025-07-13 08:41:22 +0800

    Now displays at what page you are in in the navbar. Now able to host locally for testing

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ post = Post()
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts = post.get_all_blog_post())
+    return render_template("index.html", posts=post.get_all_blog_post(), page_name="Home")
 
 @app.route('/post/<blog_num>')
 def blog(blog_num):
-    return render_template("post.html", blog=post.get_specific_post(blog_num))
+    return render_template("post.html", blog=post.get_specific_post(blog_num), page_name="Blog")
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_name="Contact Me")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_name="About Me")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0",port=5000)
